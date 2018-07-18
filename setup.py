c7303a27e2997b527ed92704ca65fba020a28135
Aurlus Wedava <aurlus.ismael@twigafoods.com>
2018-07-18 13:15:24 +0300

    Changing release number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mpesa-py",
-    version="0.0.2",
+    version="0.0.3",
     author="Aurlus Ismael Wedava",
     author_email="arlusishmael@gmail.com",
     description="A Python wrapper for Mpesa Daraja APIs.",
