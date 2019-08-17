b2e918e87ef47774d1554bedbaaca6ab9f5e008d
Aurlus Wedava <arlusishamel@gmail.com>
2019-08-17 11:28:33 +0300

    Updating dependencies with security vulnerabilities urllib3 - https://nvd.nist.gov/vuln/detail/CVE-2019-11324 requests - https://nvd.nist.gov/vuln/detail/CVE-2018-18074

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ),
     install_requires=[
-        'certifi', 'chardet==3.0.4', 'future==0.16.0', 'idna==2.7', 'requests==2.19.0',
-        'six==1.11.0', 'urllib3==1.23', 'pytest==3.6.1 '
+        'certifi', 'chardet==3.0.4', 'future==0.16.0', 'idna==2.7', 'requests==2.20.0',
+        'six==1.11.0', 'urllib3==1.24.2', 'pytest==3.6.1 '
     ],
 )
